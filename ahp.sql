852da0a4cc918c1e7123df38aefd51870f31dc56
Harry Witriyono <49424961+HarryWitriyono@users.noreply.github.com>
2022-06-16 14:58:57 +0700

    Add files via upload

--- ahp.sql
+++ ahp.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 16 Jun 2022 pada 04.30
--- Versi Server: 10.1.21-MariaDB
--- PHP Version: 7.1.1
+-- Generation Time: Jun 16, 2022 at 09:58 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,32 +24,71 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pengguna`
+-- Table structure for table `kriteria`
+--
+
+CREATE TABLE `kriteria` (
+  `KodeKriteria` int(11) NOT NULL,
+  `NamaKriteria` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kriteria`
+--
+
+INSERT INTO `kriteria` (`KodeKriteria`, `NamaKriteria`) VALUES
+(1, 'Kemampuan Mengajar'),
+(2, 'Sikap'),
+(3, 'Jumlah Karya');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengguna`
 --
 
 CREATE TABLE `pengguna` (
   `NamaPengguna` varchar(50) NOT NULL,
   `NamaLengkap` varchar(50) NOT NULL,
   `Password` varchar(50) NOT NULL,
-  `TanggalDaftar` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `TanggalDaftar` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `pengguna`
+-- Dumping data for table `pengguna`
 --
 
 INSERT INTO `pengguna` (`NamaPengguna`, `NamaLengkap`, `Password`, `TanggalDaftar`) VALUES
-('Admin', 'Admin SPK AHP', '1q2w3e4raz', '2022-06-16 08:54:34');
+('Admin', 'Admin SPK AHP', '1q2w3e4raz', '2022-06-16 08:54:34'),
+('Harry', 'Harry Witriyono', '1qaz2ws', '2022-06-16 12:19:13'),
+('Harry Witriyono', 'Harry Witriyono', '1qaz2wsx3edc', '2022-06-16 14:35:27');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `kriteria`
+--
+ALTER TABLE `kriteria`
+  ADD PRIMARY KEY (`KodeKriteria`);
+
+--
 -- Indexes for table `pengguna`
 --
 ALTER TABLE `pengguna`
   ADD PRIMARY KEY (`NamaPengguna`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kriteria`
+--
+ALTER TABLE `kriteria`
+  MODIFY `KodeKriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
---
+++ kriteria.php
@@ -0,0 +1,84 @@
+<?php 
+if (!isset($_SESSION)) session_start();
+$sessionname='ahp'.date('Ymd');
+if (empty($_SESSION[$sessionname])) {
+	echo "<script>window.location.href='index.php';</script>";
+	exit();
+}
+$kon=mysqli_connect("localhost","root","","ahp");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Tabel Kriteria - SPK Metode AHP</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="bootstrap.min.css" rel="stylesheet">
+  <script src="bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<div class="container mt-3">
+  <h2>Manajemen Tabel Kriteria</h2>
+  <form action="" method="post">
+    <div class="mb-3 mt-3">
+      <label for="NamaPengguna">Nama Kriteria:</label>
+      <input type="text" class="form-control" id="NamaKriteria" placeholder="Enter Nama kriteria keputusannya" name="NamaKriteria" autocomplete="off" required>
+    </div>
+	<button type="submit" class="btn btn-primary" name="BSimpan">Simpan</button>
+  </form>
+</div>
+<div class="container mt-3">
+  <h2>Tabel Kriteria</h2>
+  <p>Daftar Kriteria Sistem Pendukung Keputusan yang tersimpan :</p>            
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>Kode Kriteria</th>
+        <th>Nama Kriteria</th>
+        <th>Aksi Rekord</th>
+      </tr>
+    </thead>
+<?php
+ $sqlkriteria="select * from kriteria";
+ $qkriteria=mysqli_query($kon,$sqlkriteria);
+ $rkriteria=mysqli_fetch_array($qkriteria);
+?>
+    <tbody>
+<?php 
+ if (!empty($rkriteria)) { 
+ do { ?>
+      <tr>
+        <td><?php echo $rkriteria['KodeKriteria'];?></td>
+        <td><?php echo $rkriteria['NamaKriteria'];?></td>
+        <td>
+		<button type="submit" class="btn btn-primary" name="BKoreksi">Koreksi</button>
+		<button type="submit" class="btn btn-danger" name="BHapus">Hapus</button>
+		</td>
+      </tr>
+<?php 
+    } while ($rkriteria=mysqli_fetch_array($qkriteria));
+ }
+?>
+    </tbody>
+  </table>
+</div>
+</body>
+</html>
+<?php if (isset($_POST['BSimpan'])) {
+	$NamaKriteria=filter_var($_POST['NamaKriteria'],FILTER_SANITIZE_STRING);
+	$sql="insert into kriteria (NamaKriteria) values 
+	      ('".$NamaKriteria."');";
+	$q=mysqli_query($kon,$sql);
+	if ($q) {
+		echo '<div class="alert alert-success alert-dismissible">
+  <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+  <strong>Record Kriteria Sudah Tersimpan!</strong>.
+</div>';
+	} else {
+		echo '<div class="alert alert-danger alert-dismissible">
+  <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+  <strong>Record Kriteria Gagal Tersimpan!</strong>.
+</div>';
+	}
+ echo "<script>window.location.href='kriteria.php';</script>";
+}
