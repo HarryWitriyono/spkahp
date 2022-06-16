494d2c28055294e91798b2b0d8944a1fd712e05a
Harry Witriyono <49424961+HarryWitriyono@users.noreply.github.com>
2022-06-16 09:13:08 +0700

    Add files via upload

--- menuutama.php
+++ menuutama.php
@@ -45,8 +45,10 @@
 </nav>
 
 <div class="container-fluid mt-3">
-  <h3>Navbar Forms</h3>
-  <p>You can also include forms inside the navigation bar.</p>
+  <h3>Contoh Perhitungan SPK Metode AHP</h3>
+  <p>Aplikasi ini adalah contoh pembelajaran kecerdasan tiruan dalam bidang Sistem Pendukung Keputusan (SPK) yang metode perhitungannya dengan Analytical Hirarchie Process (AHP). Selamat belajar dan bila ada yang mau didiskusikan silahkan kontak saya di Whatsapp / Telegram : 08153902534.<br>
+  Jangan lupa subscribe, comment dan like di channel saya : <a href="https://www.youtube.com/harrywitriyono" target="_blank">https://www.youtube.com/harrywitriyono</a></p>
+  <p><iframe width="560" height="315" src="https://www.youtube.com/embed/nIctHMkDWvE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>
 </div>
 
 </body>
